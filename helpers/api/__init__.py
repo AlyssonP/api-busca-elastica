8f3aa55a912c058a10f87d662c30616e76fe6c9a
AlyssonP <alysson.nascimento@polodeinovacao.ifpb.edu.br>
2025-02-12 20:16:59 -0300

    Implementando solr

--- helpers/api/__init__.py
+++ helpers/api/__init__.py
@@ -1,7 +1,9 @@
 from flask_restful import Api
 
-from resources.OcupacaoResources import InsertOcupacoes
+from resources.OcupacaoResources import InsertOcupacoes, UpSolr, BuscadorOcupacoes
 
 api = Api()
 
 api.add_resource(InsertOcupacoes, "/insert_dataset")
+api.add_resource(UpSolr, "/up_solr")
+api.add_resource(BuscadorOcupacoes, "/ocupacoes/buscar")
--- resources/OcupacaoResources.py
+++ resources/OcupacaoResources.py
@@ -1,8 +1,12 @@
 from flask_restful import Resource
+from flask import request
 import csv
+import json
+import requests
 
 from helpers.database import db
 from models.Ocupacao import Ocupacao
+from helpers.config import URL_SOLR
 
 def dataset_ocupacao_csv(path):
     data = []
@@ -12,7 +16,6 @@ def dataset_ocupacao_csv(path):
         print(type(read))
         for row in read:
             spliter = row[0].split(";")
-            # data.append({"id":int(spliter[0]), "titulo":str(spliter[1])})
             data.append(Ocupacao(
                 id=int(spliter[0]),
                 titulo=str(spliter[1])
@@ -32,3 +35,40 @@ class InsertOcupacoes(Resource):
         except Exception as e:
             db.session.rollback()
             return {"message": "Ocorreu um erro ao tentar inserir os dados do csv", "error": str(e)}, 500
+        
+class UpSolr(Resource):
+    def post(self):
+        if(len(Ocupacao.query.all())<0):
+            return {"message":"Não há dados a ser levantados"}, 409
+
+        ocupacoes = []
+        for ocupacao  in Ocupacao.query.all():
+            ocupacoes.append({"id":ocupacao.id, "titulo":ocupacao.titulo, "full": f"{ocupacao.id} {ocupacao.titulo}"})
+
+        SOLR_URL = f"{URL_SOLR}/update?commit=true"
+
+        headers = {"Content-Type": "application/json"}
+        response = requests.post(SOLR_URL, data=json.dumps(ocupacoes), headers=headers)
+
+        if response.status_code == 200:
+            return {"message":"Dados indexados com sucesso!"}, 200
+        else:
+            return {"message":f"Erro ao indexar: {response.text}"}, 500
+
+class BuscadorOcupacoes(Resource):
+    def get(self):
+        termo = request.args.get("q", "*:*")  # Pega o termo da query string
+        params = {
+            "q": termo+"~",
+            "wt": "json"
+        }
+
+        SOLR_QUERY_URL = f"{URL_SOLR}/select"
+
+        response = requests.get(SOLR_QUERY_URL, params=params)
+
+        if response.status_code == 200:
+            return response.json(), 200
+        else:
+            return {"erro": "Falha na busca"}, 500
+
