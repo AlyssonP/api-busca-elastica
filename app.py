6c1a065b632a54e612f82797ff4fc52d4290bff0
AlyssonP <alysson.nascimento@polodeinovacao.ifpb.edu.br>
2025-02-14 14:06:01 -0300

    Corrigindo tratamento de erro

--- app.py
+++ app.py
@@ -14,11 +14,8 @@ else:
 
 app = Flask(__name__)
 
-try:
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-except Exception as e:
-    print("Erro ao tentar conectar ao banco de dados:",e)
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app=app)
 with app.app_context():
